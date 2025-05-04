a23f1ad0cc71ad628148b11daa5c08bf1a02f28f
Ayush Saran <ayushsaran@gmail.com>
2025-05-04 19:50:49 +0530

    footer component

--- app/components/ui/footer.tsx
+++ app/components/ui/footer.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 const Footer = () => {
   return (
     <footer id="globalFooter">
-      <p>Footer</p>
+      <div className="content">
+        <div className="logo">
+          <Image src="/logo_1.svg" alt="Logo" width={100} height={100}></Image>
+          <p>Saving Tech from Landfills. Since 2025</p>
+        </div>
+      </div>
     </footer>
   );
 };
