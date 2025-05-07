7399012d27710a5cd0bd2654dd8ddbef0d4912a1
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2025-05-07 15:59:03 +0530

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { headers } from "next/headers";
+import Image from "next/image";
+
+export default async function NotFound() {
+  const headersList = await headers();
+  return (
+    <div className="w-full  flex flex-col items-center justify-center text-center py-12">
+      <Image src={"/404.svg"} alt="404" width={800} height={360} />
+      <h1 className="text-4xl">Oops!</h1>
+      <p>We could not find requested path.</p>
+      <p className="my-12">
+        <Link className="button primary" href="/">
+          Go to Home
+        </Link>
+      </p>
+    </div>
+  );
+}
