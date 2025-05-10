b9219c8f8861035714413179ea1c6150808ea7f9
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2025-05-10 13:50:58 +0530

    checkout page

--- app/checkout/[itemid]/page.tsx
+++ app/checkout/[itemid]/page.tsx
@@ -11,9 +11,12 @@ import { Item } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { trimAtSpace } from "@/app/helpers/text";
 import Link from "next/link";
-import { Coins } from "lucide-react";
+import { CircleArrowRight, Coins, House } from "lucide-react";
 import { Button } from "@/app/components/ui/button";
 import { handlePurchase } from "@/app/helpers/api";
+import { Label } from "@/app/components/ui/label";
+import { Input } from "@/app/components/ui/input";
+import Spinner from "@/app/components/ui/spinner";
 
 const CheckoutPage = () => {
   const router = useRouter();
@@ -29,6 +32,15 @@ const CheckoutPage = () => {
   const images = (itemDetails?.images as string[]) || [];
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+
+  // Payment Methods
+  const paymentMethods = [
+    { id: "gpay", name: "Google Pay", icon: "/icons/gpay.svg" },
+    { id: "visa", name: "Visa", icon: "/icons/visa.svg" },
+    { id: "mastercard", name: "MasterCard", icon: "/icons/mastercard.svg" },
+  ];
+
+  const [paymentMethod, setPaymentMethod] = useState(paymentMethods[0].id);
 
   useEffect(() => {
     if (itemid && userId) {
@@ -69,7 +81,7 @@ const CheckoutPage = () => {
     }
   }, [itemDetails, userPoints]);
 
-  if (!itemDetails) return <div>Loading...</div>;
+  if (!itemDetails) return <Spinner />;
 
   const handleSubmit = async () => {
     if (loading) return;
@@ -90,58 +102,176 @@ const CheckoutPage = () => {
   return (
     <>
       <div className="container card mx-auto">
-        <h1 className="titleUnderlined">Claim this Item</h1>
-        <div className="row">
-          <div className="listing">
-            <div className="listing-image">
-              <img src={images[0]} alt={itemDetails.title} />
-            </div>
-            <div className="listing-details">
-              <p className="category">{itemDetails.category}</p>
-              <h4>
-                <Link href={`/item/${itemDetails.id}`}>
-                  {trimAtSpace(itemDetails.title, 60)}
-                </Link>
-              </h4>
-              <p className="condition">{itemDetails.condition}</p>
-            </div>
-            <p className="pointsValue">
-              <Coins width={14} height={14} />
-              {itemDetails.pointsValue}
-            </p>
-          </div>
-          {/* Checkout ---------------------- */}
-          <div className="checkout">
-            <h2 className="title">Checkout</h2>
-            <p className="line-item">
-              <span className="item-title">Points required:</span>
-              <span className="item-value">{itemDetails.pointsValue}</span>
-            </p>
-            <p className="line-item">
-              <span className="item-title">Your points balance:</span>
-              <span className="item-value">{userPoints}</span>
-            </p>
-            {difference > 0 && (
-              <div className="balance-warning">
-                <p>You need {difference} points to complete this purchase</p>
-                <button
-                  className="primary button"
-                  onClick={() => router.push("/calculate-points")}
-                >
-                  Purchase Points
-                </button>
+        {/* Breadcrumbs ---------------------------------------  */}
+        <ul className="breadcrumbs">
+          <li>
+            <Link href="/" className="flex items-center gap-1">
+              <House width={12} />
+              Home
+            </Link>
+          </li>
+          <li>&raquo;</li>
+          <li>Checkout</li>
+        </ul>
+        <div className="content">
+          {/* Left Column: Order Details ---------------------------------------  */}
+          <div className="order-details">
+            {/* Item Details ---------------------------------- */}
+            <h3 className="title">Item Details</h3>
+            <div className="listing">
+              <div className="listing-image">
+                <img src={images[0]} alt={itemDetails.title} />
               </div>
-            )}
-            <p>
-              Shipping cost:{" "}
-              {shippingCost !== null ? `$${shippingCost}` : "Calculating..."}
-            </p>
+              <div className="listing-details">
+                <p className="category">{itemDetails.category}</p>
+                <h4>
+                  <Link href={`/item/${itemDetails.id}`}>
+                    {trimAtSpace(itemDetails.title, 60)}
+                  </Link>
+                </h4>
+                <p className="condition">{itemDetails.condition} Condition</p>
+              </div>
+            </div>
+
+            {/* Shipping Details ---------------------------------- */}
+
+            <h3 className="title">Shipping Info</h3>
+
+            <div className="shipping-details">
+              <div className="origin">
+                <p className="direction">FROM:</p>
+                <p className="pincode">{itemDetails.locationPincode}</p>
+                <p className="cityState">city, State</p>
+              </div>
+              <span className="arrow">
+                <CircleArrowRight width={20} />
+              </span>
+              <div className="destination">
+                <p className="direction">TO:</p>
+                <p className="pincode">get user pincode</p>
+                <p className="cityState">city, State</p>
+              </div>
+              <div className="shipping-cost">
+                <p className="amount">
+                  ₹{" "}
+                  {shippingCost !== null ? `${shippingCost}` : "Calculating..."}
+                </p>
+                <p className="time">~ X-X days</p>
+              </div>
+            </div>
+
+            {/* Order Summary ---------------------------------- */}
+
+            <h3 className="title">Order Summary</h3>
+
+            <div className="order-summary">
+              <p>Your Points Balance</p>
+              <p className="number pointsValue">
+                <Coins width={14} height={14} />
+                {userPoints}
+              </p>
+              <p>Item Value</p>
+              <p className="number">- {itemDetails.pointsValue}</p>
+              <p>Shipping Charges</p>
+              <p className="number">
+                ₹ {shippingCost !== null ? `${shippingCost}` : "Calculating..."}
+              </p>
+              <p className="total">To Pay</p>
+              <p className="number total">₹ {shippingCost}</p>
+            </div>
+            <div>
+              {difference > 0 && (
+                <div className="balance-warning">
+                  <p>You need {difference} points to complete this purchase</p>
+                  <button
+                    className="primary button"
+                    onClick={() => router.push("/calculate-points")}
+                  >
+                    Purchase Points
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* Right Column: Payment ---------------------- */}
+          <div className="column payment">
+            <h1 className="title">Payment Details</h1>
+            <div>
+              <p className="balance">
+                You can claim this item using your points
+              </p>
+              <p className="hint">Just pay the shipping charges</p>
+            </div>
+
+            <div className="payment-method">
+              <p className="title">Payment Method</p>
+              <ul className="payment-options">
+                {paymentMethods.map((method) => (
+                  <li
+                    key={method.id}
+                    className={paymentMethod === method.id ? "active" : ""}
+                  >
+                    <label htmlFor={method.id}>
+                      <img
+                        src={method.icon}
+                        alt={method.name}
+                        className="payment-icon"
+                      />
+                    </label>
+                    <input
+                      type="radio"
+                      id={method.id}
+                      name="payment-method"
+                      value={method.name}
+                      checked={paymentMethod === method.id}
+                      onChange={() => setPaymentMethod(method.id)}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+              {paymentMethod === "gpay" ? (
+                <div className="field">
+                  <Label htmlFor="card-number">UPI id</Label>
+                  <Input type="text" id="upi-id" name="upi-id" required />
+                </div>
+              ) : (
+                <>
+                  <div className="field">
+                    <Label htmlFor="card-number">Card Number</Label>
+                    <Input
+                      type="text"
+                      id="card-number"
+                      name="card-number"
+                      required
+                    />
+                  </div>
+                  <div className="field">
+                    <Label htmlFor="expiry-date">Expiry Date</Label>
+                    <Input
+                      type="text"
+                      id="expiry-date"
+                      name="expiry-date"
+                      required
+                    />
+                  </div>
+                  <div className="field">
+                    <label htmlFor="cvv">CVV</label>
+                    <Input type="text" id="cvv" name="cvv" required />
+                  </div>
+                </>
+              )}
+            </form>
+
             <Button
               className="primary button"
               onClick={handleSubmit}
               disabled={loading}
             >
-              Claim!
+              Pay ₹{shippingCost}
             </Button>
           </div>
         </div>
