253eeacd52d6ac90cf110b46848266de2555add4
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2025-05-16 20:08:43 +0530

    banners

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import SearchBar from "@/components/ui/searchBar";
 import ProductGrid from "@/components/ui/productGrid";
 import ItemCategories from "@/components/ui/categories";
 import { fetchListings } from "@/helpers/api";
+import Image from "next/image";
 
 export default async function HomePage() {
   const data = await fetchListings({ limit: 12 });
@@ -13,34 +14,36 @@ export default async function HomePage() {
 
   return (
     <div className={styles.container}>
-      <SearchBar />
-
-      <header className="callout callout-green">
+      {/* <header className="callout callout-green">
         <h1>Where Old Tech Finds New Homes</h1>
         <p>Earn Points. Claim Tech. Pay Only Shipping.</p>
         <button>
-          <Link href="/list-item">List Your First Device</Link>
+        <Link href="/list-item">List Your First Device</Link>
         </button>
-      </header>
+        </header> */}
+
+      <Image
+        src={"/home_banner_2.png"}
+        alt="Freecycle"
+        width={1200}
+        height={400}
+        className={"my-10"}
+      />
+      <SearchBar />
 
       <ItemCategories />
 
       <ProductGrid listings={listings} />
 
-      <section className={styles.whyJoin}>
-        <h2>Why Join Freecycle</h2>
-        <ul>
-          <li>
-            <strong>Zero Waste</strong>: Every device saved = less landfill.
-          </li>
-          <li>
-            <strong>Zero Hassle</strong>: No selling drama. No haggling.
-          </li>
-          <li>
-            <strong>All Trust</strong>: Verified users. Transparent listings.
-          </li>
-        </ul>
-      </section>
+      <Image
+        src={"/home_banner_1.png"}
+        alt="Freecycle"
+        width={1200}
+        height={400}
+        className={"my-10"}
+      />
+
+      <ProductGrid listings={listings} />
 
       <footer className={styles.tagline}>
         <p>
